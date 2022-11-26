3f0f56e3035e113d5800660e5e08a71265162119
JDhillon24 <dhillonjagdeep13@gmail.com>
2022-11-25 23:43:11 -0800

    update index.js

--- todoapp/server/index.js
+++ todoapp/server/index.js
@@ -1,11 +1,32 @@
 const express = require("express")
-
 const app = express()
+app.use(express.json())
 
-app.get("/", (req, res) => {})
+var connection = require('./database').databaseConnection
 
-app.post("/", (req, res) => {})
+app.get("/", (req, res) => {
+    let sql = 'SELECT * FROM todoapp'
+
+    connection.query(sql, (err, result) => {
+        if (err) throw err
+        //console.log(result)
+        res.send(result)
+    })
+})
+
+app.post("/", (req, res) => {
+    console.log(req.body)
+    const todo = req.body.todo
+    const postdate = req.body.postdate
+
+    connection.query('INSERT INTO todoapp (todo, postdate) VALUES (?,?)', [todo, postdate], (err, result) => {
+        if (err) throw err
+        console.log(result)
+        res.send(result)
+    })
+})
 
 app.delete("/:id", (req, res) => {})
 
 app.listen(3001, () => console.log("server is running on port 3001"))
+
