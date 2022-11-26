0d4af5b33abdd5ae0f06487f0fe2d957277c90ae
BiigMoney <64580434+OKThomas1@users.noreply.github.com>
2022-11-25 21:11:02 -0800

    api skeleton

--- django_backend/api/views.py
+++ django_backend/api/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from ast import Delete
+from rest_framework.views import APIView
+from rest_framework.parsers import FormParser, MultiPartParser
 
 # Create your views here.
+
+class NodeView(APIView):
+
+	parser_classes = [FormParser, MultiPartParser]
+
+	def get(self, request, format=None):
+		return 
+
+	def post(self, request, format=None):
+		return 
+
+
+class MySQLView(APIView):
+	parser_classes = [FormParser, MultiPartParser]
+	def get(self, request, format=None):
+		return 
+
+	def post(self, request, format=None):
+		return 
+
+
+class WebsiteView(APIView):
+	parser_classes = [FormParser, MultiPartParser]
+	def get(self, request, format=None):
+		return 
+
+	def post(self, request, format=None):
+		return 
+
+class NodeDetailView(APIView):
+	def delete(self, request, pk, format=None):
+		return
+
+class MySQLDetailView(APIView):
+	def get(self, request, pk, format=None):
+		return 
+
+	def delete(self, request, pk, format=None):
+		return
+
+class WebsiteDetailView(APIView):
+	def delete(self, request, pk, format=None):
+		return
+
+
