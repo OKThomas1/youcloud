16b599b2a91326d6b1944661eaf8f78124637bd9
stejay91 <stejay91@hotmail.com>
2022-11-26 13:31:50 -0800

    todo ui

--- todoapp/src/App.svelte
+++ todoapp/src/App.svelte
@@ -1,54 +1,32 @@
 <script>
   import svelteLogo from "./assets/svelte.svg";
   import Counter from "./lib/Counter.svelte";
-
-  /*
-  let all = [];
-  let completed = [];
-  let incompleted = [];
-
-  function addAll() {
-    all = [...all, " "];
-  }
-  function addCompleted() {
-    completed = [...completed, " "];
-  }
-  function addIncompleted() {
-    incompleted = [...incompleted, " "];
-  }
-
-  function removeAll(index) {
-    all = [...all.slice(0, index), ...all.slice(index + 1)];
-  }
-  function removeCompleted(index) {
-    completed = [...completed.slice(0, index), ...completed.slice(index + 1)];
-  }
-  function removeIncompleted(index) {
-    incompleted = [
-      ...incompleted.slice(0, index),
-      ...incompleted.slice(index + 1),
-    ];
-  }
-  */
+  import Todolist from "./Todolist.svelte";
+  let test = [
+    {
+      text: "red",
+    },
+    {
+      text: "green",
+    },
+  ];
 </script>
 
-<header>
-  <h1>ToDo App</h1>
-</header>
+<main>
+  <Todolist Todo={test} />
+</main>
 
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap");
 
   :global(*) {
     font-family: "Poppins", sans-serif;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+
+    width: 100vh;
     text-decoration: none;
     color: black;
+    background-color: #7d6e83;
+    text-align: center;
+    margin-left: 7em;
   }
-
-  /* :global(body) {
-    background-color: #555555;
-  } */
 </style>
