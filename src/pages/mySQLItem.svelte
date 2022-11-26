6e3001cdb07c83e385bdc39052acfbd9c567e1f7
EstoPeru <ebermudez.aguirre@gmail.com>
2022-11-26 00:34:03 -0800

    textarea updated and loading screen

--- src/pages/mySQLItem.svelte
+++ src/pages/mySQLItem.svelte
@@ -1,21 +1,36 @@
 <script>
-import { Link } from "svelte-routing"
+import {Router, Link, Route} from "svelte-routing"
+	import axios from "axios"
+	import Cookies from "js-cookie"
+  let loading = true
+  let error = null
+  let database = null
+  export let params
 
-let loading = true
-let error=  null
-let database = {name: "testdb", username:"testusername", password:"huh?", id:1 }
-try {
-//let databasees = await axios.get("/api/mysql")
-}
-catch(err){
-
-}
-const submit = (event)  =>{
-
-}
-// const delete = (id) => {
-//     axios.delete(`/api/mysql/{id}`)
-// }
+  axios.get(`/api/mysql${params.id}`, {headers: {"X-CSRFTOKEN": Cookies.get("csrftoken")}}).then(res => {
+		database = res.data
+		loading = false
+	}).catch(err => {
+		console.error(err)
+		error = "Could not get user"
+		loading = false
+	})
 </script>
-
+{#if loading}
+  <div class="d-flex justify-content-center align-items-center">
+    <div class="my-5 spinner-border text-primary" style="width: 10rem; height: 10rem;" />
+  </div>
+{:else if error}
+  <h1>{error}</h1>
+{:else}
 <h1 class="display-02">{database.name}</h1>
+
+<div class="form-group">
+  <textarea class="form-control" id="exampleTextarea" rows="20" readonly></textarea>
+</div>
+
+<input type="text">
+
+<button class="btn btn-secondary" disabled>Import Script (.sql)</button>
+<button class="btn btn-primary" >Submit</button>
+{/if}
