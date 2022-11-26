b61a70dd5a3305a52555e0427c81d6e1f088fadf
stejay91 <stejay91@hotmail.com>
2022-11-25 23:44:36 -0800

    todo ui test

--- todoapp/src/App.svelte
+++ todoapp/src/App.svelte
@@ -1,45 +1,62 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import Counter from "./lib/Counter.svelte";
+
+  /*
+  let all = [];
+  let completed = [];
+  let incompleted = [];
+
+  function addAll() {
+    all = [...all, " "];
+  }
+  function addCompleted() {
+    completed = [...completed, " "];
+  }
+  function addIncompleted() {
+    incompleted = [...incompleted, " "];
+  }
+
+  function removeAll(index) {
+    all = [...all.slice(0, index), ...all.slice(index + 1)];
+  }
+  function removeCompleted(index) {
+    completed = [...completed.slice(0, index), ...completed.slice(index + 1)];
+  }
+  function removeIncompleted(index) {
+    incompleted = [
+      ...incompleted.slice(0, index),
+      ...incompleted.slice(index + 1),
+    ];
+  }
+  */
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<header>
+  <h1>ToDo App</h1>
+</header>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap");
+
+  :global(*) {
+    font-family: "Poppins", sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+    color: black;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  header {
+    width: 100%;
+    max-width: 36em;
+    margin: 0 auto;
+    padding: 5em 0;
+    text-align: left;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
+  /* :global(body) {
+    background-color: #555555;
+  } */
 </style>
