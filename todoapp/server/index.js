bfad50da89b9e35a9a56e7d97cc32c4579e67117
JDhillon24 <dhillonjagdeep13@gmail.com>
2022-11-26 14:26:49 -0800

    added get list of todo items

--- todoapp/server/index.js
+++ todoapp/server/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 app.use(express.json())
+const cors = require('cors')
+app.use(cors())
 
 var connection = require('./database').databaseConnection
 
--- todoapp/src/App.svelte
+++ todoapp/src/App.svelte
@@ -2,18 +2,30 @@
   import svelteLogo from "./assets/svelte.svg";
   import Counter from "./lib/Counter.svelte";
   import Todolist from "./Todolist.svelte";
-  let test = [
-    {
-      text: "red",
-    },
-    {
-      text: "green",
-    },
-  ];
+  import axios from "axios";
+  let loading = true
+  let error = null
+  let items = []
+  axios.get('http://localhost:3001/').then(res => {
+    items = res.data
+    loading = false
+  }).catch(err => {
+    console.error(err)
+    error = "could not get items"
+  })
 </script>
 
+{#if loading}
+  <div class="d-flex justify-content-center align-items-center">
+    <div class="my-5 spinner-border text-primary" style="width: 10rem; height: 10rem;" />
+  </div>
+{:else if error}
+  <h1>{error}</h1>
+{/if}
+
 <main>
-  <Todolist Todo={test} />
+  <Todolist Todo={items} />
+
 </main>
 
 <style>
