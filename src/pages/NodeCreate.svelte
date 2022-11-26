46f5a84dff01d2285c72cfef4bac165feb2b1b9f
EstoPeru <ebermudez.aguirre@gmail.com>
2022-11-26 13:15:41 -0800

    Loading icon for script upload

--- src/pages/NodeCreate.svelte
+++ src/pages/NodeCreate.svelte
@@ -3,7 +3,10 @@
 	import Cookies from "js-cookie"
 	import { navigate } from "svelte-routing";
 	let error = null
+	let loading = false
+	let success = false
   const submit = (event)  =>{
+		loading = true
 		event.preventDefault()
 		let {file, name} = event.target.elements
 		const formData = new FormData()
@@ -11,8 +14,11 @@
 		formData.append('name', name.value)
 		axios.post("/api/node", formData, {headers:{"X-CSRFTOKEN": Cookies.get("csrftoken")}}).then(res => {
 			console.log("success")
+			loading = false
+			success = true
 			navigate("/nodejs", {replace: true})
 		}).catch(err => {
+			loading = false
 			console.error(err)
 			error = "Error uploading script"
 		})
@@ -27,3 +33,18 @@
     <button class="btn btn-primary" type="submit">Upload</button>
   </form>
 
+	{#if loading}
+  <div class="d-flex justify-content-center align-items-center">
+    <div class="my-5 spinner-border text-primary" style="width: 10rem; height: 10rem;" />
+  </div>
+	{:else if error}
+	<div class="alert alert-dismissible alert-danger">
+		<button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+		<strong>{error}</strong> Try submitting again.
+	</div>
+	{:else if success}
+	<div class="alert alert-dismissible alert-success">
+		<button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+		<strong>Well done!</strong> You successfully uploaded a script
+	</div>
+{/if}
